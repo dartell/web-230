75ce3acc4d35aa8f4628418237556faffbc578b8
Darius Dinkins <dtdinkins@my365.bellevue.edu>
2020-02-16 18:05:33 -0500

    added assignment 1.4

---
+++ Week-1/Dinkins-assignment-1.4.js
@@ -0,0 +1,41 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Darius Dinkins   
+; Date: 16 Feb 2020
+; Modified By: Darius Dinkins
+; Description: This program demonstrates the
+; use of JavaScript variables
+;===========================================
+*/ 
+
+//start program
+
+
+
+/*
+  Expected output:
+    
+  First name
+  Last name
+  Age
+  Street
+  City
+  Zip code
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Darius";
+var lastName = "Dinkins";
+var age = 32;
+var street = "Meyers Avenue";
+var city = "Sour";
+var zipCode = 32510;
+
+//output
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(street)
+console.log(zipCode)
+// end program
--- Week-1/hello-world.js
+++ Week-1/hello-world.js
@@ -1,1 +1,13 @@
-console.log("Hello World, my name is Darius Dinkins");
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Darius Dinkins   
+; Date: 16 Feb 2020
+; Modified By: Darius Dinkins
+; Description: This program demonstrates the
+; use of JavaScript console.log
+;===========================================
+*/ 
+
+
+console.log("Hello World, my name is Darius Dinkins"); //console log command with the message "Hello World"
