053bdb8df9203aaadf89f17f2e933fcbc48855e3
Darius Dinkins <dtdinkins@my365.bellevue.edu>
2020-02-19 22:27:02 -0500

    Added assignment 1.1

---
+++ Week-1/Dinkins-assignment-1.1.js
@@ -0,0 +1,52 @@
+/*====================================
+
+;Title: Discussion 1.1
+
+;Author: Rochelle Markham
+
+;Modified by: Darius Dinkins
+
+;Date Modified: 19 Feb 2020
+
+;Date: 16 Feb 2020
+
+;Description: Values and Variables to
+
+;demonstrate finding errors in code
+
+;=====================================
+
+*/
+
+ 
+
+var one= 1;
+
+var two= 2;
+
+var three= 3;
+
+ 
+
+console.log(Boolean((one + two)*three == two + (one*three)));
+
+/*
+============================================
+; Title: Discussion 1.1
+; Author: Michelle Nesbitt
+; Date: 15 February 2020
+; Modified By: Darius Dinkins
+; Modified Date: 19 February 2020
+; Description: This program demonstrates the
+; use of errors in JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/ 
+const myVariable = 5;
+myVariable6 = 6;
+
+var sayHelloToMe = "Hello Michelle";
+
+var sum = myVariable + sayHelloToMe;
+
+console.log(sum);
