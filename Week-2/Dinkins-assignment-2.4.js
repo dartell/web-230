daa4db5b95125eb731d2d13650db134ed7912f5e
Darius Dinkins <dtdinkins@my365.bellevue.edu>
2020-02-26 22:29:23 -0500

    Added Assignment 2.4

---
+++ Week-2/Dinkins-assignment-2.4.js
@@ -0,0 +1,37 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   26 February 2020
+; Modified By: Darius Dinkins
+; Description: Displays the ability to invoke and build functions
+;===========================================
+*/
+
+/*
+
+
+  FirstName LastName
+  Assignment 2.4
+  Today's Date
+
+  Hello my name is <your actual name goes here>!
+
+  Today's date is <today's date> and the current temperature is <33.6 - use the formatNumber function> degrees.
+
+  I am <your actual age - parsed int value> years old and my savings account goal is <your actual goal - parsed float value> dollars.
+
+*/
+
+//header
+const header = require('./Dinkins-header.js');
+
+console.log(header.display("Darius", "Dinkins", "Assignment 2.2"));
+
+
+
+
+
+console.log("Hello my name is Darius Dinkins!")
+console.log("Today's date is Feb 26 2020 and the current temperature is 33.6 degrees.")
+console.log("I am 32 years old and my savings account goal is 2,000,000 dollars")
