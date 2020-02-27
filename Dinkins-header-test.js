e3a8095552851c4f98dc94c8c1317134b87f7ae2
Darius Dinkins <dtdinkins@my365.bellevue.edu>
2020-02-26 19:40:08 -0500

    Added required files and header files

---
+++ Dinkins-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:   25 June 2017
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('./Dinkins-header.js');
+
+console.log(header.display("Darius", "Dinkins", "Assignment 2.2"));
